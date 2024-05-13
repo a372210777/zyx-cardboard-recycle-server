5747c9b61584ebf91d7446d298e1aa58cee8b1f8
yangmin <yangmin@yangmin>
2024-05-13 17:14:26 +0800

    feat: 库存查询接口返回值重构

--- cardboard-recycle/src/main/java/cn/com/qjun/cardboard/rest/InventoryController.java
+++ cardboard-recycle/src/main/java/cn/com/qjun/cardboard/rest/InventoryController.java
@@ -1,9 +1,16 @@
 package cn.com.qjun.cardboard.rest;
 
 
+import cn.com.qjun.cardboard.service.BasicMaterialService;
+import cn.com.qjun.cardboard.service.BasicWarehouseService;
 import cn.com.qjun.cardboard.service.InventoryService;
+import cn.com.qjun.cardboard.service.StockManageService;
+import cn.com.qjun.cardboard.service.dto.BasicMaterialDto;
+import cn.com.qjun.cardboard.service.dto.BasicWarehouseDto;
 import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -26,10 +33,14 @@ public class InventoryController {
 
     @Resource
     private InventoryService inventoryService;
+    @Resource
+    private BasicMaterialService materialService;
+    @Resource
+    private BasicWarehouseService basicWarehouseService;
 
     @ApiOperation(value = "库存查询")
     @GetMapping(value = "query")
-    public ResponseEntity<Double> query(String stockId, String materialId, String dateTime) {
+    public ResponseEntity<Map<String, Object>> query(String stockId, String materialId, String dateTime) {
         if (StrUtil.isEmpty(stockId)) {
             throw new BadRequestException("仓库ID不能为空");
         }
@@ -40,6 +51,15 @@ public class InventoryController {
             throw new BadRequestException("统计日期不能为空");
         }
         Double result = this.inventoryService.query(stockId, materialId, dateTime);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        BasicMaterialDto materialEntity = materialService.findById(Integer.valueOf(materialId));
+        BasicWarehouseDto wareHouseEntity = this.basicWarehouseService.findById(Integer.valueOf(stockId));
+        Map<String, Object> resultMap = Maps.newHashMap();
+        resultMap.put("quantity", result);
+        resultMap.put("stockId", stockId);
+        resultMap.put("materialId", materialId);
+        resultMap.put("dateTime", dateTime);
+        resultMap.put("materialName", ObjectUtil.isNotEmpty(materialEntity) ? materialEntity.getName() : "");
+        resultMap.put("stockName", ObjectUtil.isNotEmpty(wareHouseEntity) ? wareHouseEntity.getName() : "");
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 }
